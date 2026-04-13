4b73b7b439abbba68c55a64f67baf0b84a61388c
Soljunge <horatiucode@gmail.com>
2026-04-13 20:44:09 +0300

    image

--- trenchsi/web/frontend/dist/assets/route-B92JNM4r.js
+++
@@ -1,1 +0,0 @@
-import{D as e,j as a,N as n,O as s}from"./index-OMMpXHYb.js";function r(){return e({select:t=>t.location.pathname})==="/channels"?a.jsx(n,{to:"/channels/$name",params:{name:"telegram"}}):a.jsx(s,{})}export{r as component};
